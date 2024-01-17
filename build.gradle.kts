69c484bb1856da5d783a7528f99cd980ee978aa0
eliingram17 <143021398+eliingram17@users.noreply.github.com>
2024-01-17 17:23:16 +0000

    Error testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,16 @@
 
 plugins {
     java
+    checkstyle
+}
+
+repositories {
+    mavenCentral()
+}
+
+checkstyle {
+    val archive = configurations.checkstyle.get().resolve().filter {
+        it.name.startsWith("checkstyle")
+    }
+    config = resources.text.fromArchiveEntry(archive, "google_checks.xml")
 }
